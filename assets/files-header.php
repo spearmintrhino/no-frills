57ef671fe8c0bf33d3b26e2a464e2c29dc4f5c97
Hinerangi Courtenay <hello@skymaiden.com>
2013-05-22 00:06:25 +0200

    Remove conditional comments to make X-UA-Compatible meta tag work with IE. IE-specific stylesheet added, * hack used for ie7.
    https://github.com/h5bp/html5-boilerplate/issues/1187

--- assets/files-header.php
+++ assets/files-header.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="assets/css/normalize.css" />
 <link rel="stylesheet" href="assets/css/main.css" />
+<!--[if lt IE 9]><link rel="stylesheet" href="assets/css/oldie.css"><![endif]-->
 <link rel="stylesheet" href="assets/css/print.css" media="print" />
 
 <!-- modernizr must be in header -->
--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php $page = (isset($_GET['page'])) ? $_GET['page'] : false; ?>
 
 <!DOCTYPE html>
-<!--[if IE 7]> <html class="no-js ie7 oldie" lang="fr"> <![endif]-->
-<!--[if IE 8]> <html class="no-js ie8 oldie" lang="fr"> <![endif]-->
-<!--[if IE 9]> <html class="no-js ie9" lang="fr"> <![endif]-->
-<!--[if gt IE 9]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+<html class="no-js">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Front-end template for <?php echo $page; ?> - Project name</title>
     <meta name="description" content="Lorem ipsum">
-    
-    <!-- delete if non-responsive project -->
-    <meta name="viewport" content="initial-scale=1">
     
     <?php include "assets/files-header.php"; ?>
 </head>
